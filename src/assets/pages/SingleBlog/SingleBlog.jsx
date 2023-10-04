58eb41905b035b768a4f20d26b3aec1466730cbe
gangapandey <igangapandey@gmail.com>
2023-10-05 00:06:54 +0545

    deleteblog

--- src/assets/pages/SingleBlog/SingleBlog.jsx
+++ src/assets/pages/SingleBlog/SingleBlog.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../../components/Navbar'
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './SingleBlog.css'
 
 const SingleBlog = () => {
+
+  const navigate = useNavigate()
 
   const {id} = useParams();
   console.log(id)
@@ -17,6 +19,21 @@ const SingleBlog = () => {
     if (response.status == 200) {
       setBlogs(response.data)
       console.log(blogs)
+    } 
+    else {
+      alert("something went wrong")
+    }
+  }
+
+  //delete blog
+  const deleteBlog = async()=>{
+    const response = await axios.delete("https://651baa58194f77f2a5aeace7.mockapi.io/blogs/" + id)
+    console.log(response)
+    if (response.status == 200){
+      navigate("/")
+    } 
+    else {
+      alert("something went wrong")
     }
   }
 
@@ -37,7 +54,7 @@ const SingleBlog = () => {
             <h2>{blogs?.title}</h2> <br/>
             <p>{blogs?.description}</p>
             <br />
-            <button onClick={()=> deleteBlog()} style={{backgroundColor:'#157fea', color: '#fff'}}>Delete Blog</button>
+            <button onClick={deleteBlog} style={{backgroundColor:'#157fea', color: '#fff'}}>Delete Blog</button>
         </div>
     </section>
 
